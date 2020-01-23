1e5917e7b0e83e8a72a29e7deab0f12a55853c81
burdukowsky <burdukowskystas@gmail.com>
2020-01-23 10:05:27 +0700

    add options menu with two items and empty handlers
    + update dependencies

--- app/src/main/java/tk/burdukowsky/enter_leave/MainActivity.kt
+++ app/src/main/java/tk/burdukowsky/enter_leave/MainActivity.kt
@@ -2,8 +2,12 @@ package tk.burdukowsky.enter_leave
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ListView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ActionDeleter {
 
@@ -14,11 +18,31 @@ class MainActivity : AppCompatActivity(), ActionDeleter {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         actions = ActionDao.queryForAll()
         lvActions = findViewById(R.id.lvActions)
         actionListAdapter = ActionListAdapter(this, actions)
         lvActions.adapter = actionListAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.actionDeleteAllButLast -> {
+                Toast.makeText(this, "DeleteAllButLast", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.actionDeleteAll -> {
+                Toast.makeText(this, "DeleteAll", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     @Suppress("UNUSED_PARAMETER")
